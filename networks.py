e9e4859a08bfd7a71eadf6f1fbdfe006fcec5d72
Vivek Tiwari <tiwarivivekraj17@gmail.com>
2021-02-02 22:20:19 +0530

    replaced view with torch.reshape

--- networks.py
+++ networks.py
@@ -121,7 +121,7 @@ class FeatureRegression(nn.Module):
 
     def forward(self, x):
         x = self.conv(x)
-        x = x.view(x.size(0), -1)
+        x = torch.reshape(x, (x.size(0), -1))
         x = self.linear(x)
         x = self.tanh(x)
         return x
